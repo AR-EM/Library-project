b85ee73b9e716b7aadf0fd9f84f279209fac237a
AR-EM <178697567+AR-EM@users.noreply.github.com>
2026-02-04 00:40:47 +0530

    Added functionality to the New Book button.

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-const newCard = document.querySelector(".newbtn button")
-const dialog = document.querySelector("dialog")
-const closeDia = document.querySelector(".dialog-container .X")
+const newCard = document.querySelector(".newbtn button");
+const dialog = document.querySelector("dialog");
+const closeDia = document.querySelector(".dialog-container .X");
+const subBtn = document.querySelector(".dialog-container #submitbtn");
+const addBookForm = document.querySelector(".add-Book-Form")
 
-//, title, number of pages, whether it’s been read and anything else you might want
-const myLibrary = []
 
-function Book(author, title, pages){
-    if (!new.target){
-        throw Error("You must use 'new' operator to call the constructor.")
-    }
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.ID = crypto.randomUUID()
+const authorName = document.querySelector("#author");
+const bookName = document.querySelector("#title");
+const noOfPages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+
+const myLibrary = [];
+
+function Book(author, title, pages, read) {
+  if (!new.target) {
+    throw Error("You must use 'new' operator to call the constructor.");
+  }
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+  this.ID = crypto.randomUUID();
 }
 
-function addBookToLibrary(author, title, pages){
-    myLibrary.push(new Book(author, title, pages))
+function addBookToLibrary(author, title, pages, read) {
+  myLibrary.push(new Book(author, title, pages, read));
 }
 
-function display(){
-    
+function display() {
+  console.log(myLibrary);
 }
 
 newCard.addEventListener("click", (event) => {
-    dialog.showModal()
-})
+  dialog.showModal();
+});
 closeDia.addEventListener("click", (event) => {
-    dialog.close()
-})
+  dialog.close();
+});
+
+addBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const author = authorName.value;
+  const title = bookName.value;
+  const pages = noOfPages.value;
+  const completed = read.checked;
+  addBookToLibrary(author, title, pages, completed);
+  display();
+  addBookForm.reset();  
+  dialog.close();
+});
